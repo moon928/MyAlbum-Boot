e71490161058ab8b89134329fc9f9d75992b31e7
Yan <1243359973@qq.com>
2020-06-04 16:31:02 +0800

    del test

--- myalbum-commons-mapper/src/main/java/cn/yan_wm/myalbum/commons/mapper/ServiceListMapper.java
+++ myalbum-commons-mapper/src/main/java/cn/yan_wm/myalbum/commons/mapper/ServiceListMapper.java
@@ -1,7 +1,7 @@
 package cn.yan_wm.myalbum.commons.mapper;
 
-import cn.yan_wm.myalbum.commons.domain.ServiceList;
+import cn.yan_wm.myalbum.commons.domain.TbService;
 import tk.mybatis.mapper.MyMapper;
 
-public interface ServiceListMapper extends MyMapper<ServiceList> {
+public interface ServiceListMapper extends MyMapper<TbService> {
 }
